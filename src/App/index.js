fcda06bf282d43986d221a7874c642a4319a2089
Alex Reznichenko <alex.reznichenko789@gmail.com>
2019-12-01 22:56:37 +0200

    Add routing and organization page

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Profile from '../Profile';
+import Organization from '../Organization';
+import * as routes from '../constants/routes';
+import './style.css';
 
-const App = props => {
-  return <Profile />;
+const WrappedOrganization = () => (
+  <div className="App-content_large-header">
+    <Organization />
+  </div>
+);
+
+const WrappedProfile = () => (
+  <div className="App-content_small-header">
+    <Profile />
+  </div>
+);
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-main">
+          <Route exact path={routes.ORGANIZATION} component={WrappedOrganization} />
+          <Route exact path={routes.PROFILE} component={WrappedProfile} />
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/Organiztion/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Organization = () => <div>Organization</div>;
+export default Organization;
