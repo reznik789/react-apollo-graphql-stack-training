47d2034830eeb80ed5bc40badc1dbff63b887ef3
Alex Reznichenko <alex.reznichenko789@gmail.com>
2019-12-22 23:30:00 +0200

    temp commit

---
+++ src/Comments/CommentsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import gql from 'graphql-tag';
+
+const GET_COMMENTS_FOR_ISSUE = gql``;
+
+const CommentsList = () => {
+  return <div className="Comments">Comments will be here</div>;
+};
+
+export default CommentsList;
--- src/Issue/IssueItem/index.js
+++ src/Issue/IssueItem/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Link from '../../Link';
+import Button from '../../Button';
+import Comments from '../../Comments';
 import './style.css';
-const IssueItem = ({ issue }) => (
-  <div className="IssueItem">
-    {/* placeholder to add a show/hide comment button later */}
-    <div className="IssueItem-content">
-      <h3>
-        <Link href={issue.url}>{issue.title}</Link>
-      </h3>{' '}
-      <div dangerouslySetInnerHTML={{ __html: issue.bodyHTML }} />
-      {/* placeholder to render a list of comments later */}
+const IssueItem = ({ issue }) => {
+  const [isCommentsVisible, setIsCommentsVisible] = useState(false);
+  const toggleComments = useCallback(() => setIsCommentsVisible(prevState => !prevState), []);
+  return (
+    <div className="IssueItem">
+      <Button onClick={toggleComments}>{isCommentsVisible ? 'Hide Comments' : 'Show Comments'}</Button>
+      <div className="IssueItem-content">
+        <h3>
+          <Link href={issue.url}>{issue.title}</Link>
+        </h3>{' '}
+        <div dangerouslySetInnerHTML={{ __html: issue.bodyHTML }} />
+        {isCommentsVisible && <Comments />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default IssueItem;
